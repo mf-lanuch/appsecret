dc860ea334386b6aecdce750bb3bd56dd0804cba
crsu <289435209@qq.com>
2019-12-23 11:39:16 +0800

    edit：获取员工个人加班时长

--- src/main/java/com/app/secret/core/dto/GetPersonalOvertimeDTO.java
+++ src/main/java/com/app/secret/core/dto/GetPersonalOvertimeDTO.java
@@ -2,50 +2,33 @@ package com.app.secret.core.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import java.util.Date;
 
 /**
  * @author crsu 2019/12/21
  */
+@Data
 public class GetPersonalOvertimeDTO {
-    @NotNull(message = "开始日期不能为空")
-    @ApiModelProperty(value = "开始日期")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date workStartDate;
+    @NotNull(message = "workYearStart不能为空")
+    @ApiModelProperty("起始日期_工作年份")
+    private Integer workYearStart;
 
-    @NotNull(message = "结束日期不能为空")
-    @ApiModelProperty(value = "结束日期")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date workEndDate;
+    @NotNull(message = "workMonthStart不能为空")
+    @ApiModelProperty("起始日期_工作月份")
+    private Integer workMonthStart;
+
+    @NotNull(message = "workYearEnd不能为空")
+    @ApiModelProperty("终止日期_工作年份")
+    private Integer workYearEnd;
+
+    @NotNull(message = "workMonthEnd不能为空")
+    @ApiModelProperty("终止日期_工作月份")
+    private Integer workMonthEnd;
 
     @ApiModelProperty(value = "工号")
     @NotBlank(message = "工号不能为空")
     private String pcode;
-
-    public Date getWorkStartDate() {
-        return workStartDate;
-    }
-
-    public void setWorkStartDate(Date workStartDate) {
-        this.workStartDate = workStartDate;
-    }
-
-    public Date getWorkEndDate() {
-        return workEndDate;
-    }
-
-    public void setWorkEndDate(Date workEndDate) {
-        this.workEndDate = workEndDate;
-    }
-
-    public String getPcode() {
-        return pcode;
-    }
-
-    public void setPcode(String pcode) {
-        this.pcode = pcode;
-    }
 }
